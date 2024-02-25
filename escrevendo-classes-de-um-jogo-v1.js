dac6ba6f5bb3a8e014a61dd029f46df2c9d755dc
Ivo C. Neto <135380558+icneto603@users.noreply.github.com>
2024-02-25 20:16:50 -0300

    Update escrevendo-classes-de-um-jogo-v1.js
    Código inserido

--- escrevendo-classes-de-um-jogo-v1.js
+++ escrevendo-classes-de-um-jogo-v1.js
@@ -1,1 +1,29 @@
+//Classe que recebe os dados do herói.
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  //Método atacar utiliza o vetor ataque para comparar o tipo de herói.
+  atacar() {
+    let ataque = ["Magia", "Espada", "Artes Marciais", "Shuriken"];
+
+    if (this.tipo === "Mago") {
+      console.log(`O ${this.tipo} atacou usando ${ataque[0]}.`);
+    } else if (this.tipo === "Guerreiro") {
+      console.log(`O ${this.tipo} atacou usando ${ataque[1]}.`);
+    } else if (this.tipo === "Monge") {
+      console.log(`O ${this.tipo} atacou usando ${ataque[2]}.`);
+    } else {
+      console.log(`O ${this.tipo} atacou usando ${ataque[3]}.`);
+    }
+  }
+}
+
+//Instanciando um novo objeto(heroi)
+let heroiA = new Heroi("Subzero", 500, "Guerreiro");
+console.log(heroiA);
+heroiA.atacar();
 
